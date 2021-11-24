6b8d7a22a6394e250f04948fde42760c37a3618d
reanto guerra <91695120+renatowar@users.noreply.github.com>
2021-11-24 12:23:04 -0300

    modificações no websocket

--- app.py
+++ app.py
@@ -1,80 +1,88 @@
 from flask import Flask, flash, render_template
-from flask_socketio import SocketIO, emit, send
+from flask_socketio import SocketIO, emit, join_room
+import random 
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 app.config['SECRET_KEY'] = 'secret!'
 
-#login
-@app.route('/login')
-def login():
- return render_template('login.html')
+class nada():
+    def __init__():
+        sala = str(random.random())
 
-#socketio login
-@socketio.on('login')
-def socketio_login(login_senha):
-    senha = login_senha['senha']
-    login = login_senha['login']
-    try:
-        with open(f'{login}.txt', 'r') as arquivo:
-            senha_teste = arquivo.read()
-    except:
-        socketio.emit('erro', 'erro')
-    else:
-        if senha == senha_teste:
-            socketio.emit('cookie_', 'cookie')
-            socketio.emit('login_aprovado', 'login aprovado')
+
+    #login
+    @app.route('/login')
+    def login():
+        return render_template('login.html')
+
+    #socketio login
+    @socketio.on('login')
+    def socketio_login(login_senha):
+        senha = login_senha['senha']
+        login = login_senha['login']
+        codigo = str(login_senha['codigo'])
+        print(codigo)
+        try:
+            with open(f'{login}.txt', 'r') as arquivo:
+                senha_teste = arquivo.read()
+        except:
+            socketio.emit(codigo+'erro', 'erro')
         else:
-            socketio.emit('erro', 'erro')
+            if senha == senha_teste:
+                socketio.emit(codigo+'cookie_', 'cookie')
+                socketio.emit(codigo+'login_aprovado', 'login aprovado')
+            else:
+                socketio.emit(codigo+'erro', 'erro')
 
-#cadastro
-@app.route('/cadastro')
-def cadastro():
-    return render_template('cadastro.html')
+    #cadastro
+    @app.route('/cadastro')
+    def cadastro():
+        return render_template('cadastro.html')
 
-#socketio Cadastro
-@socketio.on('cadastro')
-def socketio_cadastro(login_senha):
-    senha = login_senha['_senha_']
-    login = login_senha['_login_']
-    try:
-        with open(f'{login}.txt', 'r') as arquivo:
-            teste = arquivo.read()
-    except:
-        with open(f'{login}.txt', 'w') as arquivo:
-            arquivo.write(senha)
-        socketio.emit('login_aprovado_', 'login aprovado')
-        socketio.emit('cookie_', 'cookie')
-    else:
-        socketio.emit('cadastro_erro', 'erro')
+    #socketio Cadastro
+    @socketio.on('cadastro')
+    def socketio_cadastro(login_senha):
+        senha = login_senha['_senha_']
+        login = login_senha['_login_']
+        try:
+            with open(f'{login}.txt', 'r') as arquivo:
+                teste = arquivo.read()
+        except:
+            with open(f'{login}.txt', 'w') as arquivo:
+                arquivo.write(senha)
+            socketio.emit('login_aprovado_', 'login aprovado')
+            socketio.emit('cookie_', 'cookie')
+        else:
+            socketio.emit('cadastro_erro', 'erro')
 
-#banco
-@app.route('/banco')
-def banco():
-    return render_template('banco.html')
+    #banco
+    @app.route('/banco')
+    def banco():
+        return render_template('banco.html')
 
-@socketio.on('pegar_valor_da_conta')
-def pegar_valor_da_conta(username):
-    try:
-        with open(f'{username}.txt', 'r') as arquivo:
-            valor = arquivo.read()
-    except:
-        print(username)
-        pass
+    @socketio.on('pegar_valor_da_conta')
+    def pegar_valor_da_conta(username):
+        try:
+            with open(f'{username}.txt', 'r') as arquivo:
+                valor = arquivo.read()
+        except:
+            print(username)
+            pass
 
-#redirecionar 
-@app.route('/')
-def redirecionar():
-    return """
-    <script>
-        let url = document.location
-        window.location.href = url+'login';
-    </script>
-    """
+    #redirecionar 
+    @app.route('/')
+    def redirecionar():
+        return """
+        <script>
+            let url = document.location
+            window.location.href = url+'login';
+        </script>
+        """
 
-@socketio.on('conectado')
-def conectado(msg):
-    print(msg)
+    @socketio.on('conectado')
+    def conectado(msg):
+        print(msg)
 
-if __name__ == "__main__":    
-    socketio.run(app, debug=True)
+    if __name__ == "__main__":    
+        socketio.run(app)
